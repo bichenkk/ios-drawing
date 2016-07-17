97e4be037bd5bd15f474f61feb738515e239df6b
B Chen Ka Kit <bichenkk@gmail.com>
2016-07-17 17:08:05 +0800

    Initial Commit

---
+++ Drawing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Drawing
+//
+//  Created by KK Chen on 7/17/16.
+//  Copyright © 2016 KK Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Drawing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Drawing
+//
+//  Created by KK Chen on 7/17/16.
+//  Copyright © 2016 KK Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
